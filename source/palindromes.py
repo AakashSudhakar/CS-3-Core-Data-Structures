266cd298f7104767ac67113da2b8450b232c20e6
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-02-28 16:10:21 -0800

    Updated palindromes code with TODOs

--- source/palindromes.py
+++ source/palindromes.py
@@ -5,39 +5,40 @@ from re import sub
 
 def is_palindrome(text):
     """ Function checks if input string is palindrome (ignores non-alphabetical characters). """
-    # implement is_palindrome_iterative and is_palindrome_recursive below, then
-    # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), "\nINPUT IS NOT A STRING {}".format(text)
     # return is_palindrome_iterative(text)
     return is_palindrome_recursive(text)
 
 def is_palindrome_iterative(text):
     """ Iteratively checks using RegEx for alphabetical matching across string by index. """
+    # TODO: Don't do RegEx!
     text = sub(r"[^a-zA-Z]", "", text).lower()              # RegEx comparative search 
     left, right = 0, len(text) - 1
 
-    # While indices are unequal & converging, check left-right char match (mid doesn't matter)
-    while (right > left):
-        print("{} ?= {}".format(text[left], text[right]))
+    # While indices are unequal & converging, check L/R char match (mid doesn't matter)
+    while right > left:
+        print("\n{} ?= {}".format(text[left], text[right]))
         if text[left] != text[right]:
             return False
+        # Increment left bound and decrement right bound to check next L/R char match
         left += 1
         right -= 1
     return True
 
 def is_palindrome_recursive(text, left=None, right=None):
     """ Recursively checks using RegEx for alphabetical matching across string by index. """
-    text = sub(r"[^a-zA-Z]", "", text).lower()              # RegEx comparative search 
+    # TODO: Don't do RegEx!
+    text = sub(r"[^a-zA-Z]", "", text).lower()              # RegEx comparative search
     
     if left is None: left = 0
     if right is None: right = len(text) - 1
 
-    # While indices are unequal & converging, check left-right char match (mid doesn't matter)
-    while (right > left):
-        print("{} ?= {}".format(text[left], text[right]))
+    # While indices are unequal & converging, check L/R char match (mid doesn't matter)
+    while right > left:
+        print("\n{} ?= {}".format(text[left], text[right]))
         if text[left] != text[right]:
             return False
-        # If left-right char match, recursively call function with incremented/decremented L/R indices
+        # If L/R char match, recursively call function with incremented/decremented L/R indices
         return is_palindrome_recursive(text, left + 1, right - 1)
     return True
 
