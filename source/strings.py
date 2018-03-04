f0c58cfcfa68d042e111fb96630a06721fc4f191
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-03-04 13:09:41 -0800

    Modified some output code for strings.py

--- source/strings.py
+++ source/strings.py
@@ -65,7 +65,7 @@ def test_string_algorithms(text, pattern):
 
 
 def main():
-    """Read command-line arguments and test string searching algorithms."""
+    """ Reads command-line arguments and test string searching algorithms. """
     import sys
     args = sys.argv[1:]  # Ignore script file name
     if len(args) == 2:
@@ -74,12 +74,12 @@ def main():
         test_string_algorithms(text, pattern)
     else:
         script = sys.argv[0]
-        print('Usage: {} text pattern'.format(script))
-        print('Searches for occurrences of pattern in text')
-        print("\nExample: {} 'abra cadabra' 'abra'".format(script))
-        print("contains('abra cadabra', 'abra') => True")
-        print("find_index('abra cadabra', 'abra') => 0")
-        print("find_all_indices('abra cadabra', 'abra') => [0, 8]")
+        print("\nUSAGE: {} => '[text] [pattern]'".format(script))
+        print("SEARCHES FOR OCCURRENCES OF PATTERN IN TEXT")
+        print("\nEXAMPLE: {} 'abra cadabra' 'abra'".format(script))
+        print(" > contains('abra cadabra', 'abra') => True")
+        print(" > find_index('abra cadabra', 'abra') => 0")
+        print(" > find_all_indices('abra cadabra', 'abra') => [0, 8]\n")
 
 
 if __name__ == '__main__':
